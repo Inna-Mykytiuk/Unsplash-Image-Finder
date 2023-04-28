da5f95d07a6318719257c2e32387f330fa780f52
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-04-28 11:15:17 +0300

    update
    delete trash

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import { AppStyled } from './App.styled';
-// import { ImageGallery } from './ImageGallery/ImageGallery';
 import { UnsplashGallery } from './Unsplash/UnsplashGallery/UnsplashGallery';
 import React from 'react';
 
 export const App = () => {
   return (
     <AppStyled>
-      {/* <ImageGallery /> */}
       <UnsplashGallery />
     </AppStyled>
   );
