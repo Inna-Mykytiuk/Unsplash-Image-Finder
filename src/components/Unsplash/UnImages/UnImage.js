887c0e792ce9c1010f2a5e3d163c1a134f66dce0
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-04-28 12:32:55 +0300

    add key id

--- src/components/Unsplash/UnImages/UnImage.js
+++ src/components/Unsplash/UnImages/UnImage.js
@@ -1,8 +1,8 @@
 import { Image } from './UnImage.styled';
 
-export const UnImage = ({ tags, largeImage, openModal }) => {
+export const UnImage = ({ tags, largeImage, openModal, id }) => {
   return (
-    <div onClick={() => openModal({ largeUrl: largeImage, tags })}>
+    <div onClick={() => openModal({ largeUrl: largeImage, tags, id })} key={id}>
       <Image
         src={largeImage}
         alt={tags}
